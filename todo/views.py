a6e23b099b0cccca7b0530e2c0fe414cbbfd5484
patriciomirosh <pato.16.pp@gmail.com>
2023-01-06 22:11:30 -0300

    Create and read done,  view agregar completed

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+
+from .models import Tarea
+from .forms import TareaForm
+
+
+def home(request):
+    tareas = Tarea.objects.all()
+    context = {'tareas':tareas}
+    return render(request, 'todo/home.html', context)
+
+
+
+def agregar(request):
+    if request.method == "POST":
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TareaForm()
+    
+    context =  {'form': form}
+    return render(request, 'todo/agregar.html',context)
+
+def eliminar(request, tarea_id):
+    tarea = Tarea.objects.get(id=tarea_id)
+    tarea.delete()
+    return redirect("home")
 
 
 
